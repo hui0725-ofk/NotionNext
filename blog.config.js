938fbe6736ecadc5e0c6a30a6bdab56c309c0ba2
hui0725-ofk <2418786154@qq.com>
2024-10-09 17:28:38 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -243,12 +243,7 @@ const BLOG = {
   // 鼠标点击烟花特效
   FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || true, // 开关
   // 烟花色彩，感谢 https://github.com/Vixcity 提交的色彩
-  FIREWORKS_COLOR: [
-    '255, 20, 97',
-    '24, 255, 146',
-    '90, 135, 255',
-    '251, 243, 140'
-  ],
+  FIREWORKS_COLOR: ['255, 20, 97','24, 255, 146','90, 135, 255','251, 243, 140'],
 
   // 鼠标跟随特效
   MOUSE_FOLLOW: process.env.NEXT_PUBLIC_MOUSE_FOLLOW || true, // 开关
